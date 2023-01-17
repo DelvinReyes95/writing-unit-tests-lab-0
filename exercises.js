2836babfb5c622429704d0ac7b05db0eebef6e5e
Delvin Reyes <DelvinReyes.95@Gmail.com>
2023-01-17 17:14:15 -0500

    solved testing unit-0

--- exercises.js
+++ exercises.js
@@ -1,45 +1,105 @@
 // Question 1
-const isRealPalindrome = () => {
 
+const isRealPalindrome = (str) => {
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  return str === str.split("").reverse().join("");
 };
 
 // Question 2
-const runningTotal = () => {
 
+const runningTotal = (numbers) => {
+  let total = 0;
+  return numbers.map(function (num) {
+    total += num;
+    return total;
+  });
 };
 
 // Question 3
-const swap = () => {
 
+const swap = (reverse) => {
+  return reverse.split(' ').map((word) => {
+    if (word.length > 1) { 
+      word = word[word.length - 1] + word.substring(1, word.length - 1) + word[0];
+    }
+    return word;
+    })
+    .join(' ');
+  // let words = str.split(" ");
+  // for (let i = 1; i < words.length; i++) {
+  //     let word = words[i];
+  //     let firstLetter = word[0];
+  //     let lastLetter = word[word.length - 1];
+  //     words[i] = lastLetter + word.slice(i, -1) + firstLetter;
+      
+  // }
+  // console.log(words.join(" "));
+  
 };
 
 // Question 4
-const wordSizes = () => {
 
+const wordSizes = (str) => {
+  let wordCount = {};
+  let words = str.split(" ");
+  
+  for (let i = 0; i < words.length; i++) {
+    let length = words[i].length;
+    if (wordCount[length]) {
+      wordCount[length]++;
+    } else {
+      wordCount[length] = 1;
+    }
+  }
+  
+  return wordCount;
+  
 };
 
 // Question 5
-const union = () => {
 
+const union = (arr1, arr2) => {
+  let set = new Set(arr1.concat(arr2));
+    return [...set];
 };
 
 // Question 6
-const firstRecurring = () => {
 
+const firstRecurring = (str) => {
+  let charMap = {};
+  for (let i = 0; i < str.length; i++) {
+    if (charMap[str[i]]) {
+      return str[i];
+    } else {
+      charMap[str[i]] = true;
+    }
+  }
+  return "";
 };
 
 // Question 7
-const showMultiplicativeAverage = () => {
-
+const showMultiplicativeAverage = (arr) => {
+  let product = arr.reduce((prev, current) => prev * current);
+  return (product / arr.length).toFixed(3); // toFixed(3) is used to limit the decimal point to 3
 };
 
 // Quetsion 8
-const multiplyList = () => {
+
+const multiplyList = (arr1, arr2) => {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    result.push(arr1[i] * arr2[i]);
+  }
+  return result;
 };
 
 // Question 9
-const sequence = () => {
-
+const sequence = (num) => {
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+  return arr;
 };
 
 // The below code connects this JS file to our test file
